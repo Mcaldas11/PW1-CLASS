33b33d1dd96c3ef789474cd8f292cc36da402e99
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-10-03 09:32:57 +0100

    pw1 class

---
+++ watch-list-app/src/App.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+  data(){
+    return {
+      message: 'Hello World!',
+      greeting: 'my first vue app!',
+      movieName: '',
+      movieList: [],
+    }
+  },
+  methods:{
+    addMovie(){
+      this.movieList.push(this.movieName);
+      this.movieName = '';
+      console.log(JSON.stringify(this.movieList));
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+  <h1>{{ message }}</h1>
+  <p>
+    {{ greeting }}
+  </p>
+
+
+  <div class ="movie-forms">
+  <input type="text" placeholder="Nome do filme" v-model="movieName">
+
+  <button @click="addMovie">Adicionar</button>
+  </div>
+  <h2 id="movie-list">Movie List</h2>
+
+  <ul aria-labelledby="movie-list">
+    <li v-for="movie in movieList" :key="movie">{{ movie }}</li>
+  </ul>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  color:red;
+}
+
+div.movie-forms {
+  display: flex;
+  gap: 10px;
+  margin-top: 20px;
+}
+</style>
