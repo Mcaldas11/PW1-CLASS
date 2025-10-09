959b309ced618aa9de6dca0ad7ffa5378bcf563c
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-10-09 14:53:05 +0100

    ex02

--- ficha01/src/App.vue
+++ ficha01/src/App.vue
@@ -1,15 +1,18 @@
 <script >
 import Ex01 from './componentes/Ex01.vue'
+import Ex02 from './componentes/Ex02.vue'
 
 export default {
   components: {
-    Ex01
+    Ex01,
+    Ex02
   }
 }
 </script>
 
 <template>
   <Ex01 />
+  <Ex02 />
 </template>
 
 <style scoped></style>
---
+++ ficha01/src/componentes/Ex02.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="intro">
+		<h2>First name: {{ firstName }}</h2>
+		<h2>Last name: {{ lastName }}</h2>
+		<h2>Age: {{ age }}</h2>
+		<h2>Full name: {{ fullName() }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Ex02Component',
+	data() {
+		return {
+			firstName: 'John',
+			lastName: 'Doe',
+			age: 30
+		}
+	},
+	methods: {
+		fullName() {
+			return this.firstName + ' ' + this.lastName
+		}
+	}
+}
+</script>
+
+<style scoped></style>
