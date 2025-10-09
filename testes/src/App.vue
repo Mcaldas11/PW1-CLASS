1e7c3a9df737c27bef6c6ae2f91f52c3139caa35
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-10-09 14:18:07 +0100

    fichas

---
+++ testes/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <div>
+    {{ count }}
+  </div>
+  <p>dobro: {{ double() }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 1,
+    }
+  },
+  methods: {
+    double() {
+      return this.count * 2
+    },
+  },
+  beforeCreate() {
+    console.log(`beforeCreate ${this.count}`)
+  },
+  created() {
+    console.log(`created ${this.count}`)
+  },
+}
+</script>
+
+<style scoped></style>
