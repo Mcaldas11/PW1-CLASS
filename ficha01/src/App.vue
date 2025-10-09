daa0324d97f7fe6eb0845d84f589b747886a9a5f
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-10-09 15:07:44 +0100

    ex03

--- ficha01/src/App.vue
+++ ficha01/src/App.vue
@@ -1,11 +1,13 @@
 <script >
 import Ex01 from './componentes/Ex01.vue'
 import Ex02 from './componentes/Ex02.vue'
+import Ex03 from './componentes/Ex03.vue'
 
 export default {
   components: {
     Ex01,
-    Ex02
+    Ex02,
+    Ex03,
   }
 }
 </script>
@@ -13,6 +15,7 @@ export default {
 <template>
   <Ex01 />
   <Ex02 />
+  <Ex03 />
 </template>
 
 <style scoped></style>
---
+++ ficha01/src/componentes/Ex03.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>Peso: {{weight}} kgs</h2>
+</template>
+
+<script>
+export default {
+  name: "Ex03Component",
+  data() {
+    return {
+      weight: 72
+    }
+  },
+  created(){
+    console.log(`created ${this.weight}`)
+  },
+}
+</script>
+
+<style scoped></style>
