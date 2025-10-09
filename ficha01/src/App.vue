0f60e4c7928478c04bfce67f5425dbd2fb575e08
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-10-09 15:52:36 +0100

    ficha 1 completa

--- ficha01/src/App.vue
+++ ficha01/src/App.vue
@@ -2,12 +2,14 @@
 import Ex01 from './componentes/Ex01.vue'
 import Ex02 from './componentes/Ex02.vue'
 import Ex03 from './componentes/Ex03.vue'
+import Ex04 from './componentes/Ex04.vue'
 
 export default {
   components: {
     Ex01,
     Ex02,
     Ex03,
+    Ex04,
   }
 }
 </script>
@@ -16,6 +18,7 @@ export default {
   <Ex01 />
   <Ex02 />
   <Ex03 />
+  <Ex04 />
 </template>
 
 <style scoped></style>
---
+++ ficha01/src/componentes/Ex04.vue
@@ -0,0 +1,44 @@
+<template>
+    <div v-if="availability">
+        <h2>Product Name: {{productName}}</h2>
+        <h2>Product Description:</h2>
+        <p v-html="productDesc"></p>
+        <img :src="productImage" alt="Product Image">
+        <h2 v-if="price>= 1000">Price ${{ price }}</h2>
+        <button @click="reset">reset</button>
+        <button @click="buy">Comprar</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "Ex04Product",
+        data(){
+                return {
+                 productName: "Dell XPS",
+                 productDesc: "Processador i7<br>Disco SSD 256GB<br> Monitor Samsung 21’",
+                 productImage: "https://tinyurl.com/2p9dnx68",
+                 price: 1200,
+                 availability: true
+                }
+          },
+          methods: {
+            reset(){
+                this.productName = "Dell XPS";
+                this.productDesc = "Processador i7<br>Disco SSD 256GB<br> Monitor Samsung 21’";
+                this.productImage = "https://tinyurl.com/2p9dnx68";
+                this.price = 1200;
+            },
+            buy(){
+                this.availability = false;
+                alert("Compra efetuada com sucesso!");
+            }
+          }
+    }
+
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
