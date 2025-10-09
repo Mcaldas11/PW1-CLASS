ded100516a3e10af1ad69fdcfdb2dcfa891cf487
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-10-09 14:42:42 +0100

    exercicios

---
+++ ficha01/src/App.vue
@@ -0,0 +1,15 @@
+<script >
+import Ex01 from './componentes/Ex01.vue'
+
+export default {
+  components: {
+    Ex01
+  }
+}
+</script>
+
+<template>
+  <Ex01 />
+</template>
+
+<style scoped></style>
---
+++ ficha01/src/componentes/Ex01.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+    name: "Ex01Component",
+    data() {
+        return {
+            msg: "Hello Vue!"
+        }
+    }
+}
+</script>
+
+<style scoped></style>
