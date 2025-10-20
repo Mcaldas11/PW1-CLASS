cede2594e0aab09ac2be7dbbd4c3b7e2149ea0b6
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-10-20 11:39:23 +0100

    aula04 componentes

---
+++ aula04/src/App.vue
@@ -0,0 +1,27 @@
+<script >
+import ButtonCounter from './componentes/ButtonCounter.vue';
+export default {
+  name: 'App',
+  components: {
+    ButtonCounter,
+  },
+  data() {
+    return { count: 0 }
+  },
+  methods: {
+    onCountChanged(newCount) { this.count = newCount }
+  }
+};
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <ButtonCounter @count-changed="onCountChanged" />
+  <p>Count: {{ count }}</p>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
---
+++ aula04/src/componentes/ButtonCounter.vue
@@ -0,0 +1,30 @@
+<template>
+        <div>
+            <button @click="onCount">Contar</button>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: 'ButtonCounter',
+        data() {
+            return {
+                count: 0
+            };
+        },
+        methods: {
+            increment() {
+                this.count++;
+            }
+            ,
+            onCount() {
+                this.increment();
+                this.$emit('count-changed', this.count);
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
