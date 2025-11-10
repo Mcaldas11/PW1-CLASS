782318f72e39783dc21327ee3f6ab57524569a40
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-11-10 12:11:15 +0000

    aula07

---
+++ aula07/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { useCounterStore } from '@/stores/counter.js'
+export default {
+  name: 'App',
+  data() {
+    return {
+      store: useCounterStore(),
+    }
+  },
+}
+</script>
+
+<template>
+  <h1>Counter : {{ store.count }}</h1>
+  <button @click="store.increment()">Increment</button>
+
+  <h2>Double Counter : {{ store.doubleCount }}</h2>
+
+</template>
+
+<style scoped></style>
